b1524a8f4034063021844932999dcb5758abf54e
immortalprogrammist <immortalvelial@icloud.com>
2023-02-15 12:56:42 +0100

    code refactored

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 "use strict";
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
-// Modal window
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
 
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+// Modal window
 function openModal(event) {
   event.preventDefault();
 
@@ -72,10 +78,6 @@ document
   });
 
 // Tabbed component
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
 tabsContainer.addEventListener("click", function (event) {
   const clicked = event.target.closest(".operations__tab");
 
